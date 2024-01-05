9606e55d469669ef80acee1f7ef6a34a08bfeedd
mustafah <mustafa.hoda@adyen.com>
2024-01-05 15:04:59 -0600

    message

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 export default function App() {
   return (
     <>
+      <h1>Welcome Chi-Adyeners!</h1>
       <h2>Apple Pay Test!!</h2>
     </>
   );
