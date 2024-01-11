f58b705ebdd5f5e0409587a54b181ba65108256c
mustafah <mustafa.hoda@adyen.com>
2024-01-11 11:52:51 -0600

    wokring

---
+++ public/adyenConfig.ts
@@ -0,0 +1,28 @@
+export const configuration = {
+  locale: "en_US", // The shopper's locale. For a list of supported locales, see https://docs.adyen.com/online-payments/web-components/localization-components.
+  environment: "test", // When you're ready to accept live payments, change the value to one of our live environments https://docs.adyen.com/online-payments/components-web#testing-your-integration.
+  clientKey: "test_EOBXGJE7SFCF5C7E63JM6ZNXNQX6N7LI", // Your client key. To find out how to generate one, see https://docs.adyen.com/development-resources/client-side-authentication. Web Components versions before 3.10.1 use originKey instead of clientKey.
+  analytics: {
+    enabled: true, // Set to false to not send analytics data to Adyen.
+  },
+  session: {
+    id: "",
+    sessionData: ""  },
+  showPayButton: true,
+  setStatusAutomatically: true,
+  // paymentMethodsResponse: {}, // The payment methods response returned in step 1.
+  // onChange: handleOnChange, // Your function for handling onChange event
+  onSubmit: (state: any, component: any) => {
+    console.log("On Submit Executed");
+  },
+  onAdditionalDetails: (state: any, component: any) => {
+    console.log("On Additional Details Executed");
+  },
+  paymentMethodsConfiguration: {
+    card: {
+      hasHolderName: true,
+      holderNameRequired: true,
+      // billingAddressRequired: true,
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import AdyenCheckout from "@adyen/adyen-web";
+import "@adyen/adyen-web/dist/adyen.css";
+import Button from "@mui/material/Button";
+
+const configuration = require("../public/adyenConfig.ts");
 
 // @ts-ignore
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
+  const [formData, setFormData] = useState({
+    sessionId: "",
+    sessionData: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData: any) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const createAdyenCheckout = async () => {
+    console.log(configuration.configuration);
+    configuration.configuration.session.id = formData.sessionId
+    configuration.configuration.session.sessionData = formData.sessionData
+
+    const checkout = await AdyenCheckout(configuration.configuration);
+    const dropinComponent = checkout
+      .create("dropin")
+      .mount("#dropin-container");
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Here, you can perform actions with the form data, such as making an API call
+    console.log("Form submitted:", formData);
+
+    await createAdyenCheckout()
+  };
+  
+
   return (
     <>
       <h1>Welcome Chi-Adyeners!</h1>
-      <h2>Apple Pay Test!!!</h2>
+      <h2>DropInTest</h2>
 
-      <div id="dropin-container">Hello</div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          sessionId
+          <input
+            type="text"
+            name="sessionId"
+            value={formData.sessionId}
+            onChange={handleChange}
+          ></input>
+        </label>
+        <br></br>
+        <label>
+          sessiondata
+          <input
+            type="text"
+            name="sessionData"
+            value={formData.sessionData}
+            onChange={handleChange}
+          ></input>
+        </label>
+        <br></br>
+        <button type="submit" value="Submit">
+          Update DropIn
+        </button>
+      </form>
+
+      <div id="dropin-container"></div>
     </>
   );
 }
